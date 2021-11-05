8e9fb8e033381d97f4152e7d8223293997f151a9
Magnus Lundstrom <magnuslundstromdk@gmail.com>
2021-11-06 00:05:19 +0100

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -23,7 +23,7 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
-module.exports = {
+const networks = (module.exports = {
     /**
      * Networks define how you connect to your ethereum client and let you set the
      * defaults web3 uses to send transactions. If you don't specify one truffle
@@ -41,7 +41,7 @@ module.exports = {
         // tab if you use this network and you must also set the `host`, `port` and `network_id`
         // options below to some value.
         //
-        development: {
+        ganache: {
             host: '127.0.0.1', // Localhost (default: none)
             port: 7545, // Standard Ethereum port (default: none)
             network_id: '*', // Any network (default: none)
@@ -113,4 +113,4 @@ module.exports = {
     //   }
     // }
     // }
-};
+});
