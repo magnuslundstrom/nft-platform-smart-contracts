dbcb3fc81936031f4bcd11d2695845f59ba7cb2f
magnuslundstrom <magnuslundstromdk@gmail.com>
2021-11-12 16:36:39 +0100

    auction contract + test

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -1,16 +1,124 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract Auction {
-    uint256 public minPrice;
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-    // address public contract;
-    // uint256 public tokenId;
+/* is ERC165 */
+interface ERC721 {
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 indexed _tokenId
+    );
+    event Approval(
+        address indexed _owner,
+        address indexed _approved,
+        uint256 indexed _tokenId
+    );
+    event ApprovalForAll(
+        address indexed _owner,
+        address indexed _operator,
+        bool _approved
+    );
 
-    // , address _contract, uint256 _tokenId
-    constructor(uint256 _minPrice) {
-        minPrice = _minPrice;
-        // contract = _contract;
-        // tokenId = _tokenId;
+    function balanceOf(address _owner) external view returns (uint256);
+
+    function ownerOf(uint256 _tokenId) external view returns (address);
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId,
+        bytes memory data
+    ) external payable;
+
+    function safeTransferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable;
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable;
+
+    function approve(address _approved, uint256 _tokenId) external payable;
+
+    function setApprovalForAll(address _operator, bool _approved) external;
+
+    function getApproved(uint256 _tokenId) external view returns (address);
+
+    function isApprovedForAll(address _owner, address _operator)
+        external
+        view
+        returns (bool);
+}
+
+interface ERC165 {
+    function supportsInterface(bytes4 interfaceID) external view returns (bool);
+}
+
+interface ERC721TokenReceiver {
+    function onERC721Received(
+        address _operator,
+        address _from,
+        uint256 _tokenId,
+        bytes memory _data
+    ) external returns (bytes4);
+}
+
+contract NFTPlatformAuction {
+    struct Auction {
+        address seller;
+        uint256 minPrice;
+        uint256 tokenId;
+        address NFTContractAddress;
+        // Bid[] bids;
     }
+
+    mapping(string => Auction) public auctions;
+
+    // SHOULD CHECK IF THE TOKEN ACTUALLY EXISTS LATER ON
+    // mby this should be only callable by owner since we need so custom key numbers
+    // key format: tokenId:contractAddress
+    function createAuction(
+        string calldata _key,
+        uint256 _minPrice,
+        uint256 _tokenId,
+        address _NFTContractAddress
+    ) public {
+        require(
+            ERC721(_NFTContractAddress).ownerOf(_tokenId) == msg.sender,
+            "you must own the token"
+        );
+
+        // should ensure that we are approved;
+
+        Auction storage auction = auctions[_key];
+        auction.seller = msg.sender;
+        auction.minPrice = _minPrice;
+        auction.tokenId = _tokenId;
+        auction.NFTContractAddress = _NFTContractAddress;
+    }
+
+    function buyNFT(string calldata _key) public {
+        Auction memory auction = auctions[_key];
+        ERC721(auction.NFTContractAddress).safeTransferFrom(
+            auction.seller,
+            msg.sender,
+            auction.tokenId
+        );
+
+        // it should cost money obv
+        // things to consider: should we delete from the mapping?
+    }
+
+    // // function createBid(string calldata _key) public payable {
+    // //     Auction memory auction = auctions[_key];
+    // //     Bid memory bid = Bid({bidder: msg.sender, amount: msg.value});
+    // //     uint256 length = auction.bids.length;
+    // //     auction.bids[length + 1] = bid;
+    // }
 }
--- test/auction.test.js
+++ test/auction.test.js
@@ -1,15 +1,69 @@
-const Auction = artifacts.require('Auction');
+const Auction = artifacts.require('NFTPlatformAuction');
+const MintContract = artifacts.require('Mint');
 
-contract('Auction', (accounts) => {
-    const minPrice = web3.utils.toWei('1');
+contract('NFTPlatformAuction', async (accounts) => {
+    const _minPrice = web3.utils.toWei('1');
+    const nftOwner = accounts[1];
+    const tokenURI = 'https://gateway.pinata.cloud/ipfs/QmZrKyAjZjdVTyaj9nNnmP7FNT8rzQ2cEikjFwRQnPAHgW';
+    let auctionContract;
+    let mintContract;
+
+    beforeEach(async () => {
+        auctionContract = await Auction.new();
+        mintContract = await MintContract.new();
+    });
 
     describe('deploy and init values', async () => {
-        it('test minPrice being set correctly', async () => {
-            const contractInstance = await Auction.new(minPrice);
-            const actual = await contractInstance.minPrice();
+        beforeEach(async () => {
+            await mintContract.mintNFT(nftOwner, tokenURI);
+        });
 
-            // const actual = await contractInstance.minPrice();
-            assert(actual, minPrice, 'prices dont match');
+        it('we can create a new instance', async () => {
+            assert(auctionContract, 'contract should not be undefined');
+        });
+
+        it('we can create and get an auction', async () => {
+            try {
+                const key = `${1}:${mintContract.address}`;
+                await auctionContract.createAuction(key, _minPrice, 1, mintContract.address, {
+                    from: nftOwner,
+                });
+                const auction = await auctionContract.auctions(key);
+                const { seller, minPrice, tokenId, NFTContractAddress } = auction;
+
+                assert.equal(seller, nftOwner, 'msg.sender should match');
+                assert.equal(
+                    web3.utils.fromWei(_minPrice),
+                    web3.utils.fromWei(minPrice),
+                    'prices should match'
+                );
+                assert.equal(tokenId, 1, 'tokenIds should match');
+                assert.equal(NFTContractAddress, mintContract.address, 'nftContract addresses should match');
+            } catch (err) {
+                assert.fail(err.reason);
+            }
+        });
+
+        it('we can buy an NFT', async () => {
+            const key = `${1}:${mintContract.address}`;
+            // we need to approve the contract first
+            try {
+                await mintContract.mintNFT(nftOwner, tokenURI);
+                await mintContract.setApprovalForAll(auctionContract.address, true, { from: nftOwner });
+                await auctionContract.createAuction(key, _minPrice, 1, mintContract.address, {
+                    from: nftOwner,
+                });
+
+                await auctionContract.buyNFT(`${1}:${mintContract.address}`, { from: accounts[2] });
+
+                const owner = await mintContract.ownerOf(1);
+                assert.equal(owner, accounts[2], 'the nft should have been transfered');
+            } catch (err) {
+                console.log(err);
+                assert.fail('shouldnt fail in buy an NFT');
+            }
+
+            // so we must check for is not approved function calls, should return an error
         });
     });
 });
