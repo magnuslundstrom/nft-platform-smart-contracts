beaa2b00922db0898a7b57e94532a93fd208ea43
Magnus Lundstrom <magnuslundstromdk@gmail.com>
2021-12-10 23:53:36 +0100

    add ropsten deploy support

--- truffle-config.js
+++ truffle-config.js
@@ -23,6 +23,9 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 const networks = (module.exports = {
     /**
      * Networks define how you connect to your ethereum client and let you set the
@@ -62,14 +65,18 @@ const networks = (module.exports = {
         // },
         // Useful for deploying to a public network.
         // NB: It's important to wrap the provider as a function.
-        // ropsten: {
-        // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-        // network_id: 3,       // Ropsten's id
-        // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-        // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-        // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-        // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-        // },
+        ropsten: {
+            provider: () =>
+                new HDWalletProvider(
+                    process.env.MNEMONIC,
+                    `wss://ropsten.infura.io/ws/v3/805b0e402a44472ba1ea1ed42e5d4514`
+                ),
+            network_id: 3, // Ropsten's id
+            gas: 5500000, // Ropsten has a lower block limit than mainnet
+            confirmations: 2, // # of confs to wait between deployments. (default: 0)
+            timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+            skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+        },
         // Useful for private networks
         // private: {
         // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
